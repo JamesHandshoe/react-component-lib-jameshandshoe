56899a1c8d329f3f0d89dbe6958ca405cef52d79
James <jhandshoe@sbcglobal.net>
2018-04-13 12:01:50 -0400

    added styles for index js added example for hello world reconfigured webpack to have an aliased route

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HelloWorld from 'handshoe-lib/HelloWorld/HelloWorld';
+
+const ExampleHelloWorld = () => {
+    return <HelloWorld message="Example Message" />
+}
+
+export default ExampleHelloWorld;
